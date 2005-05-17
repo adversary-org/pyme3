d6892fff0c3cedf41dba4c25ab8608e7f2bc039c
belyi <devnull@localhost>
2005-05-17 16:49:28 +0000

    Update copyright note on simple.py

--- pyme/examples/simple.py
+++ pyme/examples/simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # $Id$
-# Copyright (C) 2002 John Goerzen
-# <jgoerzen@complete.org>
+# Copyright (C) 2005 Igor Belyi <belyi@users.sourceforge.net>
+# Copyright (C) 2002 John Goerzen <jgoerzen@complete.org>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
