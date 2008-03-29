57acb1089f5f8c24323ee62fc0a7f492a496b9c0
belyi <devnull@localhost>
2008-03-29 22:50:11 +0000

    Switch to using central location for python files (pycentral) Update docs rule to fix location of the python source files.

--- pyme/gpgme-h-clean.py
+++ pyme/gpgme-h-clean.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # $Id$
-# Copyright (C) 2004 Igor Belyi <belyi@users.sourceforge.net>
+# Copyright (C) 2004,2008 Igor Belyi <belyi@users.sourceforge.net>
 #
 #    This library is free software; you can redistribute it and/or
 #    modify it under the terms of the GNU Lesser General Public
