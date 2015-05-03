79e784bdcce1de6f7856921b5431044c62c6f015
Ben McGinnes <ben@adversary.org>
2015-05-03 10:18:40 +1000

    Fixed another implicit import by making it explicit.  Hopefully this is the last one.

--- pyme/util.py
+++ pyme/util.py
@@ -16,7 +16,7 @@
 #    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 
-import pygpgme
+from . import pygpgme
 from .errors import errorcheck
 
 def process_constants(starttext, dict):
